7e3f74927a98c8f48b312799d9f9a27ad39cbfb5
ahjinU <99533526+ahjinU@users.noreply.github.com>
2023-08-24 00:30:16 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 596 ms, Memory: 49332 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static int V, E;
+	static int[] parents;
+	static Edge[] edgeList;
+
+	static class Edge implements Comparable<Edge> {
+		int from, to, weight;
+
+		public Edge(int from, int to, int weight) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+
+	static void make() {
+		parents = new int[V + 1];
+
+		for (int i = 1; i <= V; i++) {
+			parents[i] = i;
+		}
+
+	}
+
+	static int find(int a) {
+		if (parents[a] == a)
+			return a;
+		return parents[a] = find(parents[a]);
+	}
+
+	static boolean union(int a, int b) {
+
+		int aRoot = find(a);
+		int bRoot = find(b);
+
+		if (aRoot == bRoot)
+			return false;
+
+		parents[bRoot] = aRoot;
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		V = Integer.parseInt(st.nextToken()); // 정점 수
+		E = Integer.parseInt(st.nextToken());// 간선 개수
+
+		make();
+
+		edgeList = new Edge[E];
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()); // 정점 수
+			int b = Integer.parseInt(st.nextToken());// 간선 개수
+			int c = Integer.parseInt(st.nextToken());// 간선 개수
+
+			edgeList[i] = new Edge(a, b, c);
+		}
+
+		Arrays.sort(edgeList);
+
+		long result = 0;
+		int cnt = 0;
+
+		for (Edge edge : edgeList) {
+			if (union(edge.from, edge.to)) {
+				result += edge.weight;
+				if (++cnt == V - 1)
+					break;
+			}
+		}
+
+		sb.append(result).append("\n");
+		System.out.println(sb);
+	}
+}
